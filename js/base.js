e42e728e775441a5e3f28a29c7b85035fd447be7
Simon St. Barth <data5tream@protonmail.com>
2018-12-28 16:33:55 +0100

    Minify JS

--- js/base.js
+++ js/base.js
@@ -1,11 +1,1 @@
-window.addEventListener('load', function () {
-    document.getElementById('navtoggle').addEventListener('click', function () {
-        var nav = document.getElementById('navtoggle').parentNode.childNodes[1];
-        if (nav.style.display === 'block') {
-            nav.style.display = 'none';
-        }
-        else {
-            nav.style.display = 'block';
-        }
-    });
-});
+window.addEventListener("load",function(){document.getElementById("navtoggle").addEventListener("click",function(){var nav=document.getElementById("navtoggle").parentNode.childNodes[1];"block"===nav.style.display?nav.style.display="none":nav.style.display="block"})});
