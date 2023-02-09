2a5a56719713239a9bc3f0a6bff299549b26427e
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-02-09 21:04:07 +0100

    add branding colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: '#F3F6F7',
+          100: '#E9EDF0',
+          200: '#D4DDE2',
+          300: '#BFCDD4',
+          400: '#ABBDC6',
+          500: '#96ACB8',
+          600: '#829CAA',
+          700: '#6E8C9D',
+          800: '#597B8E',
+          900: '#456B81',
+          1000: '#3D5A72'
+        }
+      }
+    },
   },
   plugins: [],
 }
