cb63fd2ea52aab1e8f9285ca0e7dc8c1e346f5e7
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-09-13 17:01:23 +0200

    add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import {mdsvex} from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+import adapter from "@sveltejs/adapter-static";
+import {vitePreprocess} from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  extensions: [".svelte", ...mdsvexConfig.extensions],
 
-	kit: {
-		adapter: adapter()
-	}
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
