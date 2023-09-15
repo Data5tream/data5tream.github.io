b9d30614e432fc01ba38aea40599be50fe78a794
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-09-15 11:41:53 +0200

    remove old links

--- src/lib/OtherLinks.svelte
+++ src/lib/OtherLinks.svelte
@@ -5,10 +5,6 @@
 <div class="container sm:mx-auto">
     <SectionHeader title="Links"/>
     <div class="flex justify-center gap-2">
-        <a href="https://mastodon.online/@data5tream">
-            <img src="https://img.shields.io/badge/Mastodon-6364FF?style=for-the-badge&logo=mastodon&logoColor=white" alt="Mastodon badge"/></a>
-        <a href="https://matrix.to/#/@data5tream:chat.blueignis.es">
-            <img src="https://img.shields.io/badge/Matrix-black?style=for-the-badge&logo=matrix&logoColor=white" alt="Matrix badge"/></a>
         <a href="https://linkedin.com/in/simonstbarth">
             <img src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn badge"/>
         </a>
