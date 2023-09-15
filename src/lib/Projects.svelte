a11a1e38fb563bd2bdf7d7393e941fb9add9a927
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-09-15 11:52:20 +0200

    add Easy Events project (without links)

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -3,19 +3,35 @@
     import SectionHeader from "$lib/components/SectionHeader.svelte";
 
     interface CodeProjectTag {
-      name: string,
-      classes: string,
+        name: string,
+        classes: string,
     }
 
     interface CodeProject {
-      title: string,
-      desc: string,
-      link: string,
-      source: string,
-      tags: Array<CodeProjectTag>,
+        title: string,
+        desc: string,
+        link: string,
+        source: string,
+        tags: Array<CodeProjectTag>,
     }
 
     const projects: Array<CodeProject> = [
+        {
+            title: 'Easy Events',
+            desc: 'An event organization platform built with Django and Svelte',
+            link: '',
+            source: '',
+            tags: [
+                {
+                    name: 'Python',
+                    classes: 'border-transparent bg-sky-700 text-white',
+                },
+                {
+                    name: 'Svelte',
+                    classes: 'border-transparent bg-orange-500 text-white',
+                },
+            ],
+        },
         {
             title: 'Simple Status Page',
             desc: 'Simple status page written in Rust and Svelte',
@@ -36,34 +52,34 @@
                 }
             ],
         },
-      {
-        title: 'Monolog web viewer',
-        desc: 'View monolog log files in the browser',
-        link: 'https://data5tream.github.io/monolog-web-viewer/',
-        source: 'https://github.com/Data5tream/monolog-web-viewer',
-        tags: [
-          {
-            name: 'Svelte',
-            classes: 'border-transparent bg-orange-500 text-white',
-          },
-          {
-            name: 'WIP',
-            classes: 'border-amber-500 text-amber-500',
-          }
-        ],
-      },
-      {
-        title: 'HQ Invoice',
-        desc: 'Generate an itemized list from HelloHQ reporting CSV',
-        link: 'https://data5tream.github.io/hq-invoice/',
-        source: 'https://github.com/Data5tream/hq-invoice',
-        tags: [
-          {
-            name: 'Vue',
-            classes: 'border-transparent bg-green-400',
-          },
-        ],
-      },
+        {
+            title: 'Monolog web viewer',
+            desc: 'View monolog log files in the browser',
+            link: 'https://data5tream.github.io/monolog-web-viewer/',
+            source: 'https://github.com/Data5tream/monolog-web-viewer',
+            tags: [
+                {
+                    name: 'Svelte',
+                    classes: 'border-transparent bg-orange-500 text-white',
+                },
+                {
+                    name: 'WIP',
+                    classes: 'border-amber-500 text-amber-500',
+                }
+            ],
+        },
+        {
+            title: 'HQ Invoice',
+            desc: 'Generate an itemized list from HelloHQ reporting CSV',
+            link: 'https://data5tream.github.io/hq-invoice/',
+            source: 'https://github.com/Data5tream/hq-invoice',
+            tags: [
+                {
+                    name: 'Vue',
+                    classes: 'border-transparent bg-green-400',
+                },
+            ],
+        },
     ]
 </script>
 
@@ -78,7 +94,7 @@
     <div class="container mx-auto py-4">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-2 sm:mx-0">
             {#each projects as project}
-                <Project {...project} />
+                <Project {...project}/>
             {/each}
         </div>
     </div>
