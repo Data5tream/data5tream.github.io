315aa6f68f8ffa5bff4b501c9c3965623a41cec6
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-09-15 11:57:58 +0200

    add GitHub profile link

--- src/lib/OtherLinks.svelte
+++ src/lib/OtherLinks.svelte
@@ -5,6 +5,9 @@
 <div class="container sm:mx-auto">
     <SectionHeader title="Links"/>
     <div class="flex justify-center gap-2">
+        <a href="https://github.com/data5tream">
+            <img src="https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white" alt="GitHub badge"/>
+        </a>
         <a href="https://linkedin.com/in/simonstbarth">
             <img src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn badge"/>
         </a>
